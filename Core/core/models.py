b481917802fc9d552bc92f5c7f4dd57d8bac8c61
miloss01 <mikicamiki.bat@gmail.com>
2022-12-27 14:17:03 +0100

    #4 Create core models

--- Core/core/models.py
+++ Core/core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Node:
+  def __init__(self, identifier, edges, data):
+    self.id = identifier
+    self.edges = edges
+    self.data = data
+    
+class Edge:
+  def __init__(self, first_node, second_node, data=None):
+    self.first_node = first_node
+    self.second_node = second_node
+    self.data = data
+
+class Graph:
+  def __init__(self, nodes, edges, data=None):
+    self.nodes = nodes
+    self.edges = edges
+    self.data = data
