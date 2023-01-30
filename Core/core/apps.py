767584dbcc5e1ae5205043e3133ffc958d22dba3
Ivan Martic <ivanmartic311@gmail.com>
2023-01-30 16:44:42 +0100

    #8 Add loading for all plugins in apps.py

--- Core/core/apps.py
+++ Core/core/apps.py
@@ -13,7 +13,17 @@ class CoreConfig(AppConfig):
 
     def ready(self):
         fsp_plugins = load_plugins("fsp_plugin")
-        self.loaded_plugins["fsp_plugin"] = fsp_plugins[0] if len(fsp_plugins) > 0 else None
+        svp_plugins = load_plugins("svp_plugin")
+        cvp_plugins = load_plugins("cvp_plugin")
+        twp_plugins = load_plugins("twp_plugin")
+        self.loaded_plugins["fsp_plugin"] = fsp_plugins[0] if len(
+            fsp_plugins) > 0 else None
+        self.loaded_plugins["svp_plugin"] = svp_plugins[0] if len(
+            svp_plugins) > 0 else None
+        self.loaded_plugins["cvp_plugin"] = cvp_plugins[0] if len(
+            cvp_plugins) > 0 else None
+        self.loaded_plugins["twp_plugin"] = twp_plugins[0] if len(
+            twp_plugins) > 0 else None
 
 
 def load_plugins(group):
