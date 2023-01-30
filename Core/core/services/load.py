cf348b1980254741cca14a850d8ecbafa84a80a4
Ivan Martic <ivanmartic311@gmail.com>
2023-01-30 16:46:39 +0100

    #8 Add visualize service class

--- Core/core/services/load.py
+++ Core/core/services/load.py
@@ -14,3 +14,18 @@ class LoadService(ABC):
     @abstractmethod
     def load(self):
         pass
+
+
+class VisualizeService(ABC):
+
+    @abstractmethod
+    def name(self):
+        pass
+
+    @abstractmethod
+    def identifier(self):
+        pass
+
+    @abstractmethod
+    def get_html(self, graph):
+        pass
